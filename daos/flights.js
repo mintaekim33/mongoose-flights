a7ebcbb0dde71770784f823ec59dcfd59f61c8a0
mintaekim <kmintae33@gmail.com>
2023-12-16 19:20:33 +0800

    implement get flights functionality

---
+++ daos/flights.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+  airline: {
+    type: String,
+    enum: ["American", "Asian", "European"],
+  },
+  airport: {
+    type: String,
+    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+    default: "DEN",
+  },
+  flightNo: {
+    type: Number,
+    required: true,
+    min: 10,
+    max: 9999,
+  },
+  departs: {
+    type: Date,
+    default: function () {
+      const today = new Date();
+      const nextYear = today.getFullYear() + 1;
+      return new Date(nextYear, today.getMonth(), today.getDate());
+    },
+  },
+});
